3c9f783050bcf223c8fc0b1f82bb8ef1b41e2418
Lampros Tolis <37981510+LamprosTolis@users.noreply.github.com>
2020-09-10 20:52:10 +0300

    first commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  success:string = 'Successfully alerted!';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  
+    alert(this.success);
+
+
+  }
+
+}
